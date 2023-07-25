87e1ba4f9d382083c0d32a098ffdcc8408c527f9
Pavanelli7 <vittor.ribeiro@sptech.school>
2023-07-25 10:19:38 -0300

    Dogs/Api

--- src/api/endpoints/TokenPost.jsx
+++ src/api/endpoints/TokenPost.jsx
@@ -1,54 +1,52 @@
-import React from 'react'
+import React from 'react';
 
-const UserPost = () => {
+const TokenPost = () => {
   const [username, setUsername] = React.useState('');
-  const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [token, setToken] = React.useState('');
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/api/user', {
+
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         username,
-        email,
         password,
       }),
-    }).then(response => {
-      console.log(response);
-      return response.json();
-    }).then(json => {
-      console.log(json);
-      return json;
     })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setToken(json.token)
+        return json;
+      });
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <input
         type="text"
-        placeholder='username'
+        placeholder="Username"
         value={username}
         onChange={({ target }) => setUsername(target.value)}
       />
       <input
-        type="password"
-        placeholder='password'
+        type="text"
+        placeholder="Password"
         value={password}
         onChange={({ target }) => setPassword(target.value)}
       />
-      <input
-        type="text"
-        placeholder='email'
-        value={email}
-        onChange={({ target }) => setEmail(target.value)}
-      />
       <button>Enviar</button>
+      <p style={{ wordBreak: 'break-all' }}>{token}</p>
     </form>
-  )
-}
+  );
+};
 
-export default UserPost
+export default TokenPost;
