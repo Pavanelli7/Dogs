ad95f3992848da0eb8eab86aa758b356f9395b3d
Pavanelli7 <vittor.ribeiro@sptech.school>
2023-08-03 13:52:36 -0300

    reset password

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import Input from '../Forms/Input/Input'
+import Button from '../Forms/Button/Button'
+import useForm from '../../Hooks/useForm'
+import useFetch from '../../Hooks/useFetch'
+import { PASSWORD_RESET } from '../../api'
+import Error from '../Helper/Error'
+import { useNavigate } from 'react-router-dom'
 
 const LoginPasswordReset = () => {
+  const [login, setLogin] = React.useState('');
+  const [key, setKey] = React.useState('');
+  const password = useForm();
+  const { error, loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get('key');
+    const login = params.get('login');
+    if (key) setKey(key);
+    if (login) setLogin(login);
+  }, []);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = PASSWORD_RESET({
+      login,
+      key,
+      password: password.value
+    });
+    const { response } = await request(url, options);
+    if (response.ok) navigate('/login');
+  }
+
   return (
     <div>
-
+      <h1 className='title'>Reset a Senha</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label='Nova Senha' type='password' name='password' {...password} />
+        {loading ? <Button disabled>Resetando...</Button> : <Button>Reetar</Button>}
+      </form>
+      <Error error={error} />
     </div>
   )
 }
